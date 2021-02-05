8e257fdba33d374e473eccf36136ad106b10f29b
akshaykgarg <73467753+akshaykgarg@users.noreply.github.com>
2021-02-05 18:36:42 +0400

    Max no. of items extracted
    The max items are extracted using a public function

---
+++ include/ItemManager.h
@@ -0,0 +1,11 @@
+#ifndef ITEMMANAGER_H
+#define ITEMMANAGER_H
+#include <string>
+using namespace std;
+
+class ItemManager {
+    public:
+    int GetMaxItems();
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 
 #include "MainGame.h"
-#include "Item.h"
+#include "ItemManager.h"
 
 int main()
 {
+    ItemManager myItemManager;
+    cout<<myItemManager.GetMaxItems();
     // MainGame game;
     // game.RunGame();
-    Item myItem(3);
-    cout<<myItem.GetName()<<endl<<myItem.GetAttack()<<endl<<myItem.GetDefense()<<endl<<myItem.GetHealth()<<endl<<myItem.GetCost()<<endl;
-    cout<<myItem.GetConsumable()<<endl;
+    //Item myItem(3);
+    //cout<<myItem.GetName()<<endl<<myItem.GetAttack()<<endl<<myItem.GetDefense()<<endl<<myItem.GetHealth()<<endl<<myItem.GetCost()<<endl;
+    //cout<<myItem.GetConsumable()<<endl;
     //char carray[] = {'A','B', 'C'};
     //cout<<carray[-2];
     //system("PAUSE");  
